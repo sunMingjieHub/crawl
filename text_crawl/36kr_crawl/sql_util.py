14e70b51f7046ea8ece07465b83b91ab187dadd7
smj <smjccj@163.com>
2019-11-27 10:40:04 +0800

    删除无用

--- text_crawl/36kr_crawl/sql_util.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("test.db")
-cursor = conn.cursor()
-cursor.execute("create table user( id varchar(20),name varchar(20)")
-print(cursor.rowcount)
-cursor.close()
--- toutiao_detail_artile_spider/detail_article_spider.py
+++ toutiao_detail_artile_spider/detail_article_spider.py
@@ -51,6 +51,7 @@ def get_urls():
 
 
 def get_articles(a_id):
+    rd.incr("toutiao_mark")
     global content
     options = webdriver.FirefoxOptions()
     options.add_argument('--headless')
